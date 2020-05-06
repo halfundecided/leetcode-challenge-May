e63e24c465248be56db1ca6a2450f4ced5b00207
Mijeong Ban <mban1@stevens.edu>
2020-05-05 20:27:13 -0400

    fifth commit

---
+++ 5.first-unique-character-in-a-string.py
@@ -0,0 +1,20 @@
+"""
+May 5, 2020
+First Unique Character in a String
+- Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1. 
+"""
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        dict = {}
+        for i in range(len(s)):
+            if s[i] not in dict:
+                dict[s[i]] = 1
+            else:
+                dict[s[i]] += 1
+        for i in range(len(s)):
+            if dict.get(s[i]) == 1:
+                return i
+
+        return -1
