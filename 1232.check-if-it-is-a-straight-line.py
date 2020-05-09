2ca1aa47b23a222d8397be3243f11798c35f65f6
Mijeong Ban <mban1@stevens.edu>
2020-05-08 23:50:18 -0400

    eighth commit

---
+++ 1232.check-if-it-is-a-straight-line.py
@@ -0,0 +1,22 @@
+
+class Solution:
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        if len(coordinates) == 2:
+            return True
+
+        slope = [(coordinates[1][1]-coordinates[0][1]),
+                 (coordinates[1][0]-coordinates[0][0])]
+        for i in range(len(coordinates)-1):
+            checkSlope = [(coordinates[i+1][1] - coordinates[i][1]),
+                          (coordinates[i+1][0] - coordinates[i][0])]
+            if slope[0] == 0 and checkSlope[0] == 0:
+                continue
+            if slope[1] == 0 and checkSlope[1] == 0:
+                continue
+            if ((slope[0] == 0) ^ (checkSlope[0] == 0)):
+                return False
+            if ((slope[1] == 0) ^ (checkSlope[1] == 0)):
+                return False
+            if slope[1]/slope[0] != checkSlope[1]/checkSlope[0]:
+                return False
+        return True
